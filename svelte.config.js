8affb31bf71aa53f3e59d063fb82009705a5c66e
Veron <veronika.wannack@epfl.ch>
2025-11-30 14:11:59 +0100

    config + setup

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapterStatic from "@sveltejs/adapter-static";
 import { sveltePreprocess } from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
+const dev = process.argv.includes('dev');
+
 const preprocess = sveltePreprocess({
 	postcss: {
 		plugins: [autoprefixer]
@@ -14,7 +16,10 @@ const config = {
 	},
 	preprocess,
 	kit: {
-		adapter: adapterStatic({ strict: false })
+		adapter: adapterStatic({ strict: false }),
+		paths: {
+			base: dev ? '' : '/ADA-website'
+		}
 	}
 };
 
