fbbc2289da3ff096d27d1b967230e8c3147ce166
Veron <veronika.wannack@epfl.ch>
2025-11-30 21:44:54 +0100

    Updates

--- _app/immutable/chunks/D_CK-kCr.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1mr04d9)==null?void 0:s.base)??"/ADA-website";var e;const a=((e=globalThis.__sveltekit_1mr04d9)==null?void 0:e.assets)??t??"";export{a,t as b};
