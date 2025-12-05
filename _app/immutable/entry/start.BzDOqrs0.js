e32a8aaa2fbf2d90f9528a060380753f0c8cf153
Veron <veronika.wannack@epfl.ch>
2025-12-05 18:03:10 +0100

    Updates

--- _app/immutable/entry/start.BzDOqrs0.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CjjS47Rr.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.Dd3IXriT.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DSG9nK4b.js";export{o as load_css,r as start};
