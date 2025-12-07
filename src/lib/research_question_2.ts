7aac34a5c9f347110a4abc1504d4bc0a9fa1cc3c
Kalan Raphael Walmsley <kalan.walmsley@epfl.ch>
2025-12-07 04:58:41 +0100

    progress on q2

---
+++ src/lib/research_question_2.ts
@@ -0,0 +1,96 @@
+export namespace rq2 {
+	export const repliques = {
+		"Autos & Vehicles": "Can you even drive yet?",
+		Comedy: "You definitely are funny...",
+		Education: "Are you even done with school?",
+		Entertainment: "A bit niche don't you think?",
+		"Film & Animation": "Your life might soon be a movie...",
+		Gaming: 'Oh you\'re the "never touch grass" type...',
+		"Howto & Style": "I do like your haircut...",
+		Music: "Let me guess... progressive metal?",
+		"News & Politics": "Can you even vote?",
+		"Nonprofits & Activism": "You're definitely in the no profit stage...",
+		"People & Blogs": "Ooh fancy avocado and toast!",
+		"Pets & Animals":
+			"Cats or dogs? Keep in mind there is a correct answer...",
+		"Science & Technology": "Who can be mad at that choice?!",
+		Sports: "I also like sportsball!",
+		"Travel & Events": "Oh where do you want to go?"
+	};
+
+	export const views = {
+		"Autos & Vehicles": 16075424,
+		Comedy: 24697679,
+		Education: 6809757,
+		Entertainment: 26551219,
+		"Film & Animation": 14796806,
+		Gaming: 20728262,
+		"Howto & Style": 10584777,
+		Music: 42131425,
+		"News & Politics": 31433642,
+		"Nonprofits & Activism": 4969954,
+		"People & Blogs": 8588774,
+		"Pets & Animals": 17822980,
+		"Science & Technology": 8634133,
+		Sports: 18060711,
+		"Travel & Events": 8515374,
+		All: 16481738
+	};
+
+	export const subscribers = {
+		"Autos & Vehicles": 91028,
+		Comedy: 254167,
+		Education: 77626,
+		Entertainment: 182045,
+		"Film & Animation": 104508,
+		Gaming: 119456,
+		"Howto & Style": 122232,
+		Music: 177643,
+		"News & Politics": 149547,
+		"Nonprofits & Activism": 38378,
+		"People & Blogs": 99311,
+		"Pets & Animals": 94374,
+		"Science & Technology": 58276,
+		Sports: 78487,
+		"Travel & Events": 57187,
+		All: 119154
+	};
+
+	export const videos = {
+		"Autos & Vehicles": 258,
+		Comedy: 97,
+		Education: 161,
+		Entertainment: 174,
+		"Film & Animation": 116,
+		Gaming: 308,
+		"Howto & Style": 171,
+		Music: 99,
+		"News & Politics": 1211,
+		"Nonprofits & Activism": 232,
+		"People & Blogs": 120,
+		"Pets & Animals": 177,
+		"Science & Technology": 210,
+		Sports: 272,
+		"Travel & Events": 183,
+		All: 174
+	};
+
+	export const activity = {
+		"Autos & Vehicles": 4,
+		Comedy: 3,
+		Education: 3,
+		Entertainment: 4,
+		"Film & Animation": 3,
+		Gaming: 5,
+		"Howto & Style": 3,
+		Music: 3,
+		"News & Politics": 17,
+		"Nonprofits & Activism": 3,
+		"People & Blogs": 3,
+		"Pets & Animals": 3,
+		"Science & Technology": 4,
+		Sports: 5,
+		"Travel & Events": 3,
+		All: 4
+	};
+}
